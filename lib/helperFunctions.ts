43e6725565abdc57c9a252b88edf7d97ab351218
Victoriakaey <jduan10@ucsc.edu>
2025-01-17 12:12:18 -0800

    init:

--- lib/helperFunctions.ts
+++
@@ -1,9 +0,0 @@
-import { faker } from "@faker-js/faker";
-
-export const generateFakeChartData = () => {
-  const months = ["Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct"];
-  const values = months.map(() =>
-    faker.number.int({ min: 20000, max: 100000 })
-  );
-  return { months, values };
-};
